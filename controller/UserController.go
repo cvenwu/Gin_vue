c601df7c30e165a68f7e61bfdb371e1fc195e956
yirufeng <yirufeng@foxmail.com>
2020-11-28 12:31:36 +0800

    使用jwt进行权限认证

--- controller/UserController.go
+++ controller/UserController.go
@@ -149,8 +149,6 @@ func Login(ctx *gin.Context) {
 		return
 	}
 
-
-
 	token, err := common.ReleaseToken(user)
 	if err != nil {
 		ctx.JSON(http.StatusOK, gin.H{
@@ -159,7 +157,6 @@ func Login(ctx *gin.Context) {
 		})
 		return
 	}
-
 
 	//如果正确就返回token
 	ctx.JSON(http.StatusOK, gin.H{
@@ -181,5 +178,4 @@ func Info(ctx *gin.Context) {
 			"user": user,
 		},
 	})
-
 }
